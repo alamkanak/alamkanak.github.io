23eafcf396f383dbe73a9c8457c6d111126c9025
Raquib-ul Alam <alam.kanak@gmail.com>
2020-08-27 22:42:37 +0600

    Deploy

--- assets/js/2.1a4be29d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{304:function(n,t,e){"use strict";e.r(t);var r=e(41),s=Object(r.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("    \n    Error 404\n")])}),[],!1,null,null,null);t.default=s.exports}}]);
