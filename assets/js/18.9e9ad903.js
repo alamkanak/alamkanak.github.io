bfc85731c510a9165b36650a7e3587000f49f7aa
Raquib-ul Alam <alam.kanak@gmail.com>
2020-08-29 01:39:25 +0600

    Deploy

--- assets/js/18.9e9ad903.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{322:function(t,e,n){"use strict";n.r(e);var s=n(41),r=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[e("img",{attrs:{src:"/art2.jpg",alt:"An image"}})])])}),[],!1,null,null,null);e.default=r.exports}}]);
