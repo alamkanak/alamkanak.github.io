5ec18e38dc22474624e37fb42cb85d865548ca89
Raquib Ul Alam <alam.kanak@gmail.com>
2023-01-21 20:05:02 +0000

    Deploy

--- assets/js/23.a934b318.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{263:function(t,n,s){"use strict";s.r(n);var r=s(17),e=Object(r.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[t("img",{attrs:{src:"/art5.jpg",alt:"An image"}})])])}),[],!1,null,null,null);n.default=e.exports}}]);
---
+++ assets/js/24.95c172c1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{262:function(t,n,s){"use strict";s.r(n);var r=s(17),e=Object(r.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[t("img",{attrs:{src:"/art6.jpg",alt:"An image"}})])])}),[],!1,null,null,null);n.default=e.exports}}]);
